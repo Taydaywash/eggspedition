dd495a70afba8271495f7ab8928214c3b1643d40
Taydaywash <tayvenwashington1@gmail.com>
2026-08-06 17:39:18 -0500

    added slowdown

--- States/full_egg_hop.gd
+++ States/full_egg_hop.gd
@@ -11,6 +11,8 @@ func process_physics(delta):
 	var input_direction = Input.get_axis("move_left","move_right")
 	if (abs(player.velocity.x) < move_speed) or (sign(input_direction) != sign(player.velocity.x)):
 		player.velocity.x = input_direction * move_speed
+	elif abs(player.velocity.x) > move_speed:
+		player.velocity.x = move_toward(player.velocity.x,move_speed * sign(player.velocity.x),delta*500)
 	player.velocity.y = move_toward(player.velocity.y,player.max_fall_speed,delta * player.gravity)
 	player.move_and_slide()
 	
--- States/full_egg_walk.gd
+++ States/full_egg_walk.gd
@@ -28,6 +28,8 @@ func process_physics(delta):
 	if player.velocity.x == 0 and not finishing_roll:
 		input_direction = Input.get_axis("move_left","move_right")
 		player.velocity.x = input_direction * move_speed
+	elif abs(player.velocity.x) > move_speed:
+		player.velocity.x = move_toward(player.velocity.x,move_speed * sign(player.velocity.x),delta*1000)
 	if player.velocity.y >= 1:
 		return state_machine.full_egg_fall
 	player.velocity.y = move_toward(player.velocity.y,player.max_fall_speed,delta * player.gravity)
