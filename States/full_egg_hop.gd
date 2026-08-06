91ed92a0c628fe8c184b3d51b2cf79a8157fd634
Taydaywash <tayvenwashington1@gmail.com>
2026-08-06 17:34:28 -0500

    added cracked and yolk walk animations

--- States/full_egg_hop.gd
+++ States/full_egg_hop.gd
@@ -9,7 +9,8 @@ func activate():
 
 func process_physics(delta):
 	var input_direction = Input.get_axis("move_left","move_right")
-	player.velocity.x = input_direction * move_speed
+	if (abs(player.velocity.x) < move_speed) or (sign(input_direction) != sign(player.velocity.x)):
+		player.velocity.x = input_direction * move_speed
 	player.velocity.y = move_toward(player.velocity.y,player.max_fall_speed,delta * player.gravity)
 	player.move_and_slide()
 	
