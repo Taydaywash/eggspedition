01bf7b26cd885c3524c80285c2a44144d1ee0f2a
Kellen-N <kellenswang3@gmail.com>
2026-08-06 17:23:14 -0500

    Yolk States Outline Created

--- States/state_machine.gd
+++ States/state_machine.gd
@@ -11,6 +11,11 @@ var sprite : AnimatedSprite2D
 @export var full_egg_jump : State
 @export var full_egg_fall: State
 @export var full_egg_hop: State
+@export var yolk_idle: State
+@export var yolk_walk: State
+@export var yolk_jump: State
+@export var yolk_fall: State
+@export var yolk_climb: State
 
 func initialize_state_machine() -> void:
 	for child in get_children():
