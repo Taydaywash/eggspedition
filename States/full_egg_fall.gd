e434acd02f0534589c6f16bb006ccae3df7bc65d
Kellen-N <kellenswang3@gmail.com>
2026-08-06 16:41:20 -0500

    Variables To Keep Track Of Fall Distance

--- States/full_egg_fall.gd
+++ States/full_egg_fall.gd
@@ -2,6 +2,12 @@ extends State
 
 @export var move_speed : int
 
+var peak_y: float = 0.0
+var fall_distance: float = 0.0
+
+func activate():
+	peak_y = player.global_position.y
+
 func process_physics(delta):
 	var input_direction = Input.get_axis("move_left","move_right")
 	player.velocity.x = input_direction * move_speed
@@ -11,4 +17,13 @@ func process_physics(delta):
 		if player.velocity.x:
 			return state_machine.full_egg_walk
 		return state_machine.full_egg_idle
+		
+	if player.global_position.y < peak_y:
+		peak_y = player.global_position.y
+	
 	return
+	
+func deactivate():
+	fall_distance = abs(peak_y - player.global_position.y)
+	print("Fallen Pixels: ", fall_distance)
+	peak_y = player.global_position.y
