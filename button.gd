694cf8a958b553c8acfe254f63daa089a8d78e3e
Taydaywash <tayvenwashington1@gmail.com>
2026-08-07 15:22:30 -0500

    better room stuff

--- button.gd
+++ button.gd
@@ -3,12 +3,19 @@ extends Area2D
 
 var pressed : bool = false
 @export var connected_doors : Array[ButtonDoor]
+@export var unpressed_sprite: Sprite2D
+@export var pressed_sprite: Sprite2D
+
 
 func button_pressed():
 	SignalController.emit_signal("button_pressed",connected_doors)
+	unpressed_sprite.visible = false
+	pressed_sprite.visible = true
 
 func button_unpressed():
 	SignalController.emit_signal("button_unpressed",connected_doors)
+	unpressed_sprite.visible = true
+	pressed_sprite.visible = false
 
 func _on_body_entered(_body: Node2D) -> void:
 	button_pressed()
