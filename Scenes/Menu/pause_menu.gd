31459c6e9968c21dcc76adc2e970b29986eb59d7
Kellen-N <kellenswang3@gmail.com>
2026-08-07 23:24:55 -0500

    Menu Animations Added

--- Scenes/Menu/pause_menu.gd
+++ Scenes/Menu/pause_menu.gd
@@ -9,6 +9,8 @@ func _on_resume_pressed():
 	get_tree().paused = false
 
 func _on_main_menu_pressed():
+	visible = false
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://Scenes/Menu/menu.tscn")
 
 func _input(_event: InputEvent) -> void:
