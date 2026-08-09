4b45231490076010829e9c9ed14e20acdcf29f3c
Taydaywash <tayvenwashington1@gmail.com>
2026-08-09 11:56:40 -0500

    added new music for secret room and end screen

--- Scenes/Level Editor/door.gd
+++ Scenes/Level Editor/door.gd
@@ -5,7 +5,9 @@ class_name Door
 @export var door_size : Vector2 = Vector2(300,300)
 @export var collision_shape_2d: CollisionShape2D
 var room : Room
-
+@export var play_song_on_enter : bool = false
+@export var music_player: AudioStreamPlayer2D
+@export var music: AudioStream
 @export var jump_on_enter : bool = false
 
 func _process(_delta: float) -> void:
@@ -16,6 +18,10 @@ func _ready() -> void:
 	room = get_parent()
 	
 func _on_body_entered(body: Node2D) -> void:
+	if play_song_on_enter:
+		if music_player.stream != music:
+			music_player.stream = music
+			music_player.playing = true
 	if jump_on_enter and get_parent().room_active == false:
 		body.velocity.y = -1000
 	SignalController.emit_signal("change_room",room)
