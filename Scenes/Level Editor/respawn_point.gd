ae36ff9180c641b1cb606ed64843017a877af3ce
Taydaywash <tayvenwashington1@gmail.com>
2026-08-09 19:38:46 -0500

    added checkpoint sound

--- Scenes/Level Editor/respawn_point.gd
+++ Scenes/Level Editor/respawn_point.gd
@@ -2,7 +2,8 @@ extends Area2D
 @export var ray_cast: RayCast2D
 @export var sprite: AnimatedSprite2D
 const CHECKPOINT_CONFETTI = preload("res://Assets/particles/checkpoint_confetti.tscn")
-
+const CHECKPOINT_GET = preload("res://Assets/audio/checkpoint_get.wav")
+@export var audio_controller: AudioController
 
 func _ready() -> void:
 	SignalController.connect("update_respawn_point",func(respawn_point):
@@ -17,6 +18,7 @@ func _ready() -> void:
 
 func _on_body_entered(_body: Node2D) -> void:
 	if Global.respawn_point != ray_cast.get_collision_point() - Vector2(0,84):
+		audio_controller.play_sound(CHECKPOINT_GET,1.0,1.0)
 		var particle_instance = CHECKPOINT_CONFETTI.instantiate()
 		add_child(particle_instance)
 		particle_instance.global_position = self.global_position
